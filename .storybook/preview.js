44fa549b62c4a8d802678065e7e09c8b33c57c18
givensuman <givensuman@gmail.com>
2022-08-28 16:45:56 -0500

    Configure storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
 export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
 }
