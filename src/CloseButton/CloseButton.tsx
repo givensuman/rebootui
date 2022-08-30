b2775658c52d50c2a51ecc1d7383baeb57099eed
givensuman <givensuman@gmail.com>
2022-08-30 11:24:48 -0500

    Add CloseButton

---
+++ src/CloseButton/CloseButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import cn from 'classnames'
+
+export type CloseButtonProps = {
+  white?: boolean
+} & React.HTMLAttributes<HTMLButtonElement>
+
+export const CloseButton = ({
+  white,
+  ...props
+}: CloseButtonProps) => {
+  return (
+    <button
+      className={cn('btn-close', {
+        'btn-close-white': white
+      })}
+      type="button"
+      aria-label="Close"
+      {...props}
+    />
+  )
+}
